97cef53a23f2b4e08ee30a73a87bed4def80a8ea
Szfeduś <krystianszwedo@wp.pl>
2022-04-03 15:28:23 +0200

    added empty porject

---
+++ src/test/java/com/szwedo/socialmediaservice/SocialMediaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szwedo.socialmediaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
